9666978ade7a5d8c824a9115c9733cc4b4d84329
Reverson Dias <reverson.dias@thoughtworks.com>
2021-01-10 17:29:59 -0300

    Improve to create automatic db schema

--- app/main.py
+++ app/main.py
@@ -2,6 +2,7 @@ import json
 import os
 import psycopg2
 from psycopg2 import Error
+from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
 import time
 from influxdb import InfluxDBClient
 from worker import send_metrics
@@ -12,6 +13,40 @@ def return_json(path):
         data = json.load(json_file)
 
     return(data)
+
+def db_create():
+
+
+    DB_USER = os.getenv('DB_USER')
+    DB_PASSWD = os.getenv('DB_PASSWD')
+    DB_NAME = os.getenv('DB_NAME')
+    DB_HOST = os.getenv('DB_HOST')
+    DB_PORT = os.getenv('DB_PORT',"5432")
+
+    try:
+       connection_db = psycopg2.connect(user=DB_USER, password=DB_PASSWD, host=DB_HOST, port=DB_PORT, database=DB_NAME)
+       connection_db.close()
+    except (Exception, Error):
+        connection_db_create = psycopg2.connect(user=DB_USER, password=DB_PASSWD, host=DB_HOST, port=DB_PORT)
+        connection_db_create.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
+        cursor_db_create = connection_db_create.cursor()
+        cursor_db_create.execute("CREATE DATABASE "+DB_NAME+";")
+        cursor_db_create.close()
+        connection_db_create.close()
+
+    try:
+        connection_table = psycopg2.connect(user=DB_USER, password=DB_PASSWD, host=DB_HOST, port=DB_PORT, database=DB_NAME)
+        cursor_table = connection_table.cursor()
+        cursor_table.execute("SELECT * FROM sales LIMIT 1;")
+        cursor_table.close()
+        connection_table.close()
+    except (Exception, Error):
+        connection_table_create = psycopg2.connect(user=DB_USER, password=DB_PASSWD, host=DB_HOST, port=DB_PORT)
+        connection_table_create.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
+        cursor_table_create = connection_table_create.cursor()
+        cursor_table_create.execute("CREATE TABLE sales (id SERIAL PRIMARY KEY, orderId varchar(255), statusOrder boolean, valueOrder varchar(255), clientId varchar(255), clientName varchar(255), state varchar(255), country varchar(255), phoneNumber varchar(255),statusCode int);")
+        cursor_table_create.close()
+        connection_table_create.close()
 
 def db_register(query):
 
@@ -39,7 +74,7 @@ def db_register(query):
 
 
 def app(path):
-
+    db_create()
     json_data = return_json(path)
     for el in json_data:
         query = "INSERT INTO sales (orderid, statusorder, valueorder, clientid, clientname, state, country, phoneNumber,statusCode) \
